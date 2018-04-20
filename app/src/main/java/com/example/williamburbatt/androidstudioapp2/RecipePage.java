d0c59b45bfd05ccdd7560298480cf22252317642
williamburbatt <williamburbatt@yahoo.com>
2018-04-20 14:20:16 -0500

    Finished

---
+++ app/src/main/java/com/example/williamburbatt/androidstudioapp2/RecipePage.java
@@ -0,0 +1,10 @@
+package com.example.williamburbatt.androidstudioapp2;
+
+import android.os.Bundle;
+
+public class RecipePage extends Recipes {
+    public void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.layout);
+    }
+}
